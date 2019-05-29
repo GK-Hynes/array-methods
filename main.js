0f455b479a2cfc106eeae558adc28dd3e8ea05e7
Gerard Hynes <gkhynes@gmail.com>
2019-05-29 22:29:45 +0100

    Add filter exercises

--- main.js
+++ main.js
@@ -11,3 +11,39 @@ const companies = [
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// for loop
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
+
+// forEach - loops over an array, doesn't return anything
+companies.forEach(function(company) {
+  console.log(company);
+});
+
+// filter - returns a new array of all the elements that pass the provided test
+
+// const canDrink = ages.filter(function(age) {
+//   if (age > 18) {
+//     return true;
+//   }
+// });
+
+const canDrink = ages.filter(age => age > 18);
+console.log(canDrink);
+
+const retailCompanies = companies.filter(
+  company => company.category === "Retail"
+);
+console.log(retailCompanies);
+
+const eightiesCompanies = companies.filter(
+  company => company.start >= 1980 && company.start < 1990
+);
+console.log(eightiesCompanies);
+
+const lastedTenYears = companies.filter(
+  company => company.end - company.start >= 10
+);
+console.log(lastedTenYears);
